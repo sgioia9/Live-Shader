ad4529aab7ce7b467a8c76b366f484e37a05842e
Stefano Gioia <stefanogch@gmail.com>
2017-05-21 15:29:43 -0400

    moviendo cosas

--- SGraphic/Sources/main.cpp
+++ SGraphic/Sources/main.cpp
@@ -8,11 +8,13 @@
 #include <iostream>
 #include "shader.hpp"
 
-static GLFWwindow* init_glfw_and_create_window(const int width, const int height);
+static GLFWwindow* create_window(const int width, const int height);
+static void renderLoop(GLFWwindow*);
 
 int main() {
-  auto mWindow = init_glfw_and_create_window(mWidth, mHeight);
+  glfwInit();
 
+  auto mWindow = create_window(mWidth, mHeight);
   if (mWindow == nullptr) {
     std::cerr << "Failed to create window.\n";
     return -1;
@@ -29,23 +31,26 @@ int main() {
                                 ->addSource("simple.frag")
                                 ->build();
   shader.use();
-  return 0;
+  renderLoop(mWindow);
 
-  while (glfwWindowShouldClose(mWindow) == false) {
-    if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-      glfwSetWindowShouldClose(mWindow, true);
+  glfwTerminate();
+  return 0;
+}
+
+void renderLoop(GLFWwindow* window) {
+  while (glfwWindowShouldClose(window) == false) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      glfwSetWindowShouldClose(window, true);
 
     glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glfwSwapBuffers(mWindow);
+    glfwSwapBuffers(window);
     glfwPollEvents();
-  }   glfwTerminate();
-  return 0;
+  }   
 }
 
-GLFWwindow* init_glfw_and_create_window(const int width, const int height) {
-  glfwInit();
+GLFWwindow* create_window(const int width, const int height) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
