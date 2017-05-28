3f4c395694985c4ca4ec543a5595a0eaa32a5021
Stefano Gioia <stefanogch@gmail.com>
2017-05-28 12:28:11 -0400

    displaying ogl inside window

--- SGraphic/Sources/bigwindow.cpp
+++ SGraphic/Sources/bigwindow.cpp
@@ -6,8 +6,10 @@
 
 BigWindow::BigWindow(MainFrame* mainFrame) : _mainFrame(mainFrame) {
   oglWidget = new OglWidget();
+  oglWidget->setFixedSize(QSize(800, 600));
 
   QVBoxLayout* mainLayout = new QVBoxLayout;
+  mainLayout->setMargin(0);
   mainLayout->addWidget(oglWidget);
 
   setLayout(mainLayout);
