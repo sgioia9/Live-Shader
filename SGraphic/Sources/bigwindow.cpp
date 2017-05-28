296436e640ac614cb912da03911c82e2ad81f82a
Stefano Gioia <stefanogch@gmail.com>
2017-05-28 16:32:50 -0400

    hmmm

--- SGraphic/Sources/bigwindow.cpp
+++ SGraphic/Sources/bigwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainframe.hpp"
 #include "testwidget.hpp"
 #include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QPushButton>
 #include "editorsarea.hpp"
 
 #include "bigwindow.hpp"
@@ -9,8 +11,17 @@ BigWindow::BigWindow(MainFrame* mainFrame) : _mainFrame(mainFrame) {
   _oglWidget = new TestWidget();
   _editorsArea = new EditorsArea(this);
 
-  QHBoxLayout* mainLayout = new QHBoxLayout;
-  mainLayout->addWidget(_oglWidget);
+  QHBoxLayout* mainLayout = new QHBoxLayout();
+
+  QVBoxLayout* openglAndRenderbtnLayout = new QVBoxLayout();
+  openglAndRenderbtnLayout->addWidget(_oglWidget);
+  openglAndRenderbtnLayout->addWidget(new QPushButton(tr("Render")));
+
+  QWidget* openglAndRenderbtnWidget = new QWidget();
+  openglAndRenderbtnWidget->setMinimumSize(QSize(1000, 700));
+  openglAndRenderbtnWidget->setLayout(openglAndRenderbtnLayout);
+
+  mainLayout->addWidget(openglAndRenderbtnWidget);
   mainLayout->addWidget(_editorsArea);
 
   setLayout(mainLayout);
